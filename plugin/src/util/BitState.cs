36abdce9f5f6a4511379310d73ba2e699a4c4b27
Xenira <1288524+Xenira@users.noreply.github.com>
2024-03-06 23:22:44 +0100

    refactor(util): change bitstate to not be static

--- plugin/src/util/BitState.cs
+++ plugin/src/util/BitState.cs
@@ -2,49 +2,49 @@ namespace PiUtils.Input;
 
 public class BitState
 {
-	public static long state = 0;
-	private static long initialState = 0;
+	public long state = 0;
+	private long initialState = 0;
 
-	private static long nextNewFlag = 1;
+	private long nextNewFlag = 1;
 
-	public static long GetNextFlag()
+	public long GetNextFlag()
 	{
 		var result = nextNewFlag;
 		nextNewFlag <<= 1;
 		return result;
 	}
 
-	internal static void reset()
+	internal void reset()
 	{
 		state = initialState;
 	}
 
-	public static void setState(long newState)
+	public void setState(long newState)
 	{
 		state = newState;
 	}
 
-	public static void addState(long newState)
+	public void addState(long newState)
 	{
 		state |= newState;
 	}
 
-	public static void removeState(long newState)
+	public void removeState(long newState)
 	{
 		state &= ~newState;
 	}
 
-	public static void toggleState(long newState)
+	public void toggleState(long newState)
 	{
 		state ^= newState;
 	}
 
-	public static bool hasState(long newState)
+	public bool hasState(long newState)
 	{
 		return (state & newState) != 0;
 	}
 
-	public static bool hasAnyState(long newState)
+	public bool hasAnyState(long newState)
 	{
 		return (state & newState) != 0;
 	}
